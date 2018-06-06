869bda2590cf8ea75e646a26cfd2a97628c208ee
Richardo2016 <richardo2016@gmail.com>
2018-06-06 14:31:00 +0800

    basic types support for fibjs's internal typescript.
    rewrite source with typescript, but keep 'main' reference in package.json.
    
    add fibjs's test version in travis ci; and clean repo.

---
+++ src/modules/mysql.ts
@@ -0,0 +1,47 @@
+/// <reference path="index.d.ts" />
+/// <reference path="../../@types/index.d.ts" />
+
+import db = require('db');
+import OrmNS from 'orm';
+
+class Database implements DatabaseBase {
+    conn: OrmNS.ConnInstanceInOrmConnDriverDB;
+    opts: OrmNS.OrmConnectionOpts;
+    
+    constructor(connOpts) {
+        this.opts = connOpts;
+    }
+
+    on(ev) {}
+
+    ping(cb: Function) {
+        setImmediate(cb);
+    }
+
+    connect(cb: Function): void {
+        const that = this;
+        const openMySQL: Function = db.openMySQL
+
+        openMySQL(this.opts, function (e: Error, conn: OrmNS.ConnInstanceInOrmConnDriverDB) {
+            if (!e)
+                that.conn = conn;
+            cb(e);
+        });
+    }
+
+    query(sql: string, cb: Function) {
+        this.conn.execute(sql, cb);
+    }
+
+    execute(sql: string) {
+        return this.conn.execute(sql);
+    }
+
+    end(cb: Function) {
+        this.conn.close(cb);
+    }
+}
+
+export const createConnection = function (connOpts: OrmNS.OrmConnectionOpts) {
+    return new Database(connOpts);
+};
--- src/modules/sqlite3.ts
+++ src/modules/sqlite3.ts
@@ -1,27 +1,34 @@
-"use strict";
 /// <reference path="index.d.ts" />
-Object.defineProperty(exports, "__esModule", { value: true });
-const db = require("db");
-class Database {
+
+import db = require('db');
+import OrmNS from 'orm';
+
+export class Database implements DatabaseBase {
+    conn: OrmNS.ConnInstanceInOrmConnDriverDB;
+    
     constructor(fname) {
         this.conn = db.openSQLite(fname);
     }
-    on(ev) { }
-    all(sql, cb) {
+
+    on(ev) {}
+
+    all(sql: string, cb: Function) {
         this.conn.execute(sql, cb);
     }
-    get(sql, cb) {
+
+    get(sql: string, cb: Function) {
         this.all(sql, function (e, r) {
             if (e)
                 cb(e);
             cb(e, r[0]);
         });
     }
-    execute(sql) {
+
+    execute(sql: string) {
         return this.conn.execute(sql);
     }
+
     close() {
         this.conn.close();
     }
 }
-exports.Database = Database;
