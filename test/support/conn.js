74393ca712f8eeee159b9e64e2cb15e35670b971
Richardo2016 <richardo2016@gmail.com>
2019-03-08 18:14:25 +0800

    recovery fallback mysql conn in test.

--- test/support/conn.js
+++ test/support/conn.js
@@ -1,5 +1,7 @@
 const sqliteConn = process.env.FX_ORM_TEST_SQLITE ? 'sqlite:test.db' : ''
-const mysqlConn = 'mysql://root:@127.0.0.1:3306/fxjs-orm-test';
+let mysqlConn = process.env.FX_ORM_TEST_MYSQL_CONN;
+if ((!mysqlConn || !mysqlConn.startsWith('mysql://')))
+    mysqlConn = 'mysql://root:@127.0.0.1:3306/fxjs-orm-test';
 
 let conn = sqliteConn || mysqlConn;
 if (process.env.FX_ORM_BD_DEBUG)
