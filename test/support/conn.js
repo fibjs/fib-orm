67722ccaebf57b252c2813398e135f393dcba880
Richardo2016 <richardo2016@gmail.com>
2019-03-08 17:34:30 +0800

    use mysql as default test db in travis-ci.

--- test/support/conn.js
+++ test/support/conn.js
@@ -1,10 +1,7 @@
-const sqliteConn = 'sqlite:test.db'
-let mysqlConn = process.env.FX_ORM_TEST_MYSQL
+const sqliteConn = process.env.FX_ORM_TEST_SQLITE ? 'sqlite:test.db' : ''
+const mysqlConn = 'mysql://root:@127.0.0.1:3306/fxjs-orm-test';
 
-if (mysqlConn)
-    mysqlConn = mysqlConn.startsWith('mysql://') ? mysqlConn : 'mysql://root:@127.0.0.1:3306/fxjs-orm-test';
-
-let conn = mysqlConn || sqliteConn;
+let conn = sqliteConn || mysqlConn;
 if (process.env.FX_ORM_BD_DEBUG)
     conn += '?debug=1'
 
