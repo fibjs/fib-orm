785ab874852cd6fe905ee9fe6c9fd267e6487cbf
Richardo2016 <richardo2016@gmail.com>
2019-04-25 19:20:00 +0800

    test, model: add test case model-find.callback.js

--- test/index.js
+++ test/index.js
@@ -39,6 +39,7 @@ function t() {
     run('./integration/model-find-chain.js');
     run('./integration/model-find-mapsto.js');
     run('./integration/model-find.js');
+    run('./integration/model-find.callback.js');
     run('./integration/model-get.js');
     run('./integration/model-get.callback.js');
     run('./integration/model-keys.js');
