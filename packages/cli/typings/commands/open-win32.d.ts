c6d189e429a0125caace449d39a0d3f9185520b6
richardo2016 <richardo2016@gmail.com>
2022-07-19 01:01:15 +0800

    chore(*): cleanup.

--- packages/cli/typings/commands/open-win32.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (cli: import("@fxjs/cli/typings/Cli")) => import("@fxjs/cli/type").CliCommand;
-export default _default;
--- packages/orm/typings/3rd.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "events" {
-    export const EventEmitter: typeof Class_EventEmitter
-}
