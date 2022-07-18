4332cb7726f757060cd8daf542ddc69fab5fa4ec
richardo2016 <richardo2016@gmail.com>
2022-07-19 05:32:23 +0800

    test: little fix, ensure returned data stable.

--- packages/orm/test/integration/instance-issues.js
+++ packages/orm/test/integration/instance-issues.js
@@ -4,14 +4,14 @@ test.setup();
 var helper = require('../support/spec_helper');
 var ORM = require('../../');
 
-describe("Model instance", function () {
+describe("Model instance issues", function () {
     var db = null;
     var Person = null;
 
     var setup = function () {
         db.settings.set('instance.returnAllErrors', true);
 
-        Person = db.define("person", {
+        Person = db.define("person_issues", {
             id: {
                 type: 'serial',
             },
@@ -49,7 +49,7 @@ describe("Model instance", function () {
 
     describe("issues", function () {
         it("set instance field by named accessor", function () {
-            var person = Person.find().firstSync();
+            var person = Person.find({}, { order: 'id' }).firstSync();
 
             assert.equal(person.id, 1);
             assert.equal(person.name, 'Jeremy Doe');
@@ -62,23 +62,23 @@ describe("Model instance", function () {
         });
 
         it("disallow set serial-type field to another number", function () {
-            var person = Person.find().firstSync();
-            assert.equal(person.id, 1);
+            var person = Person.find({}, { order: 'id' }).firstSync();
+            assert.strictEqual(person.id, 1);
 
             person.id = 2;
 
-            assert.equal(person.id, 1);
+            assert.strictEqual(person.id, 1);
         });
 
         it("allow set serial-type field to void value", function () {
-            var person = Person.find().firstSync();
-            assert.equal(person.id, 1);
+            var person = Person.find({}, { order: 'id' }).firstSync();
+            assert.strictEqual(person.id, 1);
 
             person.id = null;
-            assert.equal(person.id, null);
+            assert.strictEqual(person.id, null);
 
             person.id = undefined;
-            assert.equal(person.id, undefined);
+            assert.strictEqual(person.id, undefined);
         });
     })
 });
