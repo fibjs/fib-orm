ea52742e62fc1d160bbb44a1cab0bba686b1d605
richardo2016 <richardo2016@gmail.com>
2022-07-18 11:58:46 +0800

    ci: use docker to test.

--- .github/workflows/run-ci.sh
+++ .github/workflows/run-ci.sh
@@ -1,3 +1,23 @@
+PWD=$(pwd)
+
+case "${RUNNER_OS}" in
+    Windows) ;;
+    macOS)
+        brew install psqlodbc;
+        cd Dockerfile;
+        docker-compose up -d;
+        ;;
+    Linux)
+        sudo apt-get update;
+        sudo apt-get install odbc-postgresql
+        cd Dockerfile;
+        docker-compose up -d;
+        cd ..;
+        ;;
+esac
+
+cd $PWD;
+
 npm install;
 ./node_modules/.bin/lerna bootstrap;
 npm run ci;
