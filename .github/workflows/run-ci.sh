4e39a6cef6dac0fa27eaf9c8fb0869ee198ddc3a
Richard <richardo2016@gmail.com>
2022-10-01 02:24:02 +0800

    feat: upgarde ci. (#36)

--- .github/workflows/run-ci.sh
+++ .github/workflows/run-ci.sh
@@ -4,20 +4,22 @@ case "${RUNNER_OS}" in
     Windows) ;;
     macOS)
         brew install psqlodbc;
-        cd Dockerfile;
-        docker-compose up -d;
+        # cd Dockerfile;
+        # docker-compose up -d;
         ;;
     Linux)
         sudo apt-get update;
         sudo apt-get install odbc-postgresql
+        ls -la /usr/lib/x86_64-linux-gnu/odbc;
+        sudo ln -s /usr/lib/x86_64-linux-gnu/odbc/psqlodbca.so /usr/local/lib/psqlodbca.so
+        sudo ln -s /usr/lib/x86_64-linux-gnu/odbc/psqlodbcw.so /usr/local/lib/psqlodbcw.so
         cd Dockerfile;
         docker-compose up -d;
-        cd ..;
         ;;
 esac
 
 cd $PWD;
 
 npm install;
-./node_modules/.bin/lerna bootstrap;
+npm run bootstrap:noci;
 npm run ci;
